f7b7a7b6045d78daf9974bc1ddc5efc21b086942
JakubSx01 <126695976+JakubSx01@users.noreply.github.com>
2023-05-24 14:50:03 +0000

    Działa

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.util.*;
 class Main {
   public static void main(String[] args) {
-    System.out.println("TEST!");
-    System.out.println("TEST!");
+    Scanner scan = new Scanner(System.in);
+    int liczba = scan.nextInt();
+    int dlugosc = String.valueOf(liczba).length();
+   int suma=0;
+    for (int i=0; i<=dlugosc;i++){
+      int cyfra= liczba%10;
+      suma= suma + cyfra;
+      liczba=liczba/10;
+    }
+    System.out.println("Suma liczb twojej cyfry to: "+suma);
     
   }
 }
